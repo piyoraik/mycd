f2e24913966ddc9b28b5fbf52aa55c30363bc287
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-01-10 22:02:12 +0900

    56. Removing Records

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 
@@ -11,7 +11,7 @@ export class UsersController {
     this.usersService.create(body.email, body.password);
   }
 
-  @Get(':id')
+  @Get('/:id')
   findUser(@Param('id') id: string) {
     return this.usersService.findOne(parseInt(id));
   }
@@ -19,5 +19,10 @@ export class UsersController {
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.usersService.remove(parseInt(id))
   }
 }
