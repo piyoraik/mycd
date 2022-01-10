2e006dae20e8d5755a43b9168ab4d3f0a25c7694
piyoraik <42093052+piyoraik@users.noreply.github.com>
2022-01-10 22:08:09 +0900

    57. Updating Records

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,6 @@
-import { Body, Controller, Delete, Get, Param, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -19,6 +20,11 @@ export class UsersController {
   @Get()
   findAllUsers(@Query('email') email: string) {
     return this.usersService.find(email);
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string ,@Body() body: UpdateUserDto) {
+    return this.usersService.update(parseInt(id), body)
   }
 
   @Delete('/:id')
